dd2a255fba350b2f4eb9aac50304581043027d72
davidnatanel <85590345+davidnatanel@users.noreply.github.com>
2022-08-28 23:40:34 -0300

    GitFlow

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -1,77 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '../css/Contactme.module.css'
 import github from '../assets/Logos/github.svg'
 import linkedin from '../assets/Logos/linkedin.svg'
-
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contactme = () => {
+  const [state, handleSubmit] = useForm("xbjblqgr");
 
-    let [input, setInput] = useState({
-        comment: "",
-        name:"",
-        email:""
-      })
-      
+
+
+  return (
+    <div className={styled.contactme} id='Contactme'>
+      {  state.succeeded ?
     
-      function onInputChange(e) {
-        e.preventDefault()
-        setInput({
-          ...input,
-          [e.target.name]: e.target.value
-        })
-      }
+    window.location.reload()
     
-      const sendEmail=(e)=>{
-    
-    
-        e.preventDefault()
+ :null
+       }
+      <div className={styled.container}>
 
-        console.log(input)
-    
-    
-      }
-    
+        <div className={styled.box}>
 
+          <div className={styled.boxleft}>
+            <h1>Contact   <h1 style={{ display: 'inline', color: 'red', fontSize: '40PX' }}> me</h1></h1>
+            <p>  contact me
+              Thank you for coming this far, if you think I can add value to your team, do not hesitate to write me.
+            </p>
 
-    return (
-        <div className={styled.contactme} id='Contactme'>
-               <div className={styled.container}>
+            <div className={styled.ContainerDown}>
+              <a href="https://github.com/davidnatanel">
+                <img src={github} />
+              </a >
+              <a href="https://www.linkedin.com/in/david-natanael-gomez/">
 
-               <div className={styled.box}>
-             
-                <div className={styled.boxleft}>
-                    <h1>Contact   <h1 style={{display :'inline', color:'red',fontSize:'40PX'}}> me</h1></h1>
-                    <p>  contact me
-Thank you for coming this far, if you think I can add value to your team, do not hesitate to write me.
-                    </p>
+                <img src={linkedin} />
+              </a >
 
-                    <div className={styled.ContainerDown}>
-        <a href="https://github.com/davidnatanel"> 
-        <img     src={github} />
-       </a > 
-       <a href="https://www.linkedin.com/in/david-natanael-gomez/"> 
+            </div>
+          </div>
+          <div className={styled.boxrigth}>
 
-        <img  src={linkedin} />
-        </a > 
-
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email">
+                Email Address
+              </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+              <textarea
+                id="message"
+                name="message"
+              />
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+              <button type="submit" disabled={state.submitting}>
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
-                </div>
-                <div className={styled.boxrigth}>
 
-                    <form onSubmit={(e)=>sendEmail(e)}>
-                        <input     onChange={e=>onInputChange(e)} placeholder='name...' type="text" name='name'/>
-                        <input     onChange={e=>onInputChange(e)} placeholder='email...' type="text" name='email'/>
-                        <textarea  onChange={e=>onInputChange(e)} placeholder='comment...' name="comment" id="" cols="30" rows="10"></textarea>
-                        <button type='submit'>Send</button>
-                    </form>
-                </div>
-               </div>
+      </div>
 
-               </div>
 
-            
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Contactme;
